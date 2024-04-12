33f27648b730e545b6d090cb2a4cfb7828aee3a6
Linepixer <diazmatias@linepixer.com>
2024-04-12 02:25:24 +0000

    v1.2 - Dockerizacion

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Version ligera de python en docker
+FROM python:3.12.3-alpine
+
+# Seteo de directorio principal
+WORKDIR /app
+
+# Software principal y dependencias
+COPY main.py requirements.txt .
+
+# Instalacion de dependencias
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Ejecucion de la aplicacion
+CMD [ "python", "./main.py" ]
--- main.py
+++ main.py
@@ -1,43 +1,47 @@
-#!/usr/bin/python
+#!venv/bin/python
+import os
 import json
 import requests
 from time import sleep
 from godaddypy import Client, Account
 
-version = "v1.1"
+# Version del software
+version = "v1.2"
 print("Starting DomainSync " + version)
 
-public_key = ''
-secret_key = ''
+# Autentificacion
+public_key = os.getenv('PUBLIC_KEY')
+secret_key = os.getenv('SECRET_KEY')
 
+# IP inicial
+old_public_ip="0.0.0.0"
+
+# Obtener IP publica
 def get_public_ip():
 	return requests.get("https://ipinfo.io/json", verify = True).json()['ip']
 
-def get_old_public_ip():
-    json_file = open("public_ip.json", "r")
-    old_public_ip = json.load(json_file)["public_ip"]
-    json_file.close()
-    return old_public_ip
-
+# Actualizacion de DNS
 def dns_update(new_ip):
+    global old_public_ip
     client = Client(Account(api_key=public_key, api_secret=secret_key))
     if client.update_ip(new_ip, domains=['linepixer.com']) == True:
-        json_file = open("public_ip.json", "w")
-        json.dump({"public_ip":new_ip}, json_file)
-        json_file.close()
-        return
+        old_public_ip = new_ip
     else:
         raise
 
+# Bucle principal
 while True:
     try:
         public_ip = get_public_ip()
-        old_public_ip = get_old_public_ip()
         if public_ip != old_public_ip:
             dns_update(public_ip)
-            print("SUCESS: Domain updated")
+            print(f"SUCESS: Domain updated to {public_ip}")
         else:
             print("INFO: Without changes")
-    except:
+    except KeyboardInterrupt:
+        print("INFO: Stopping DomainSync")
+        break
+    except Exception as e:
         print("ERROR: Failed to perform the operation")
-    sleep(300)
+        print(e)
+    sleep(180)
